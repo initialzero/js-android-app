72fd92ab1e736133b2d5e48f0c3555be74ac87c6
Tom Koptel <tom.koptel@gmail.com>
2014-10-13 13:15:47 +0300

    Updating assets for consistent XP

--- app/src/main/java/com/jaspersoft/android/jaspermobile/activities/repository/adapter/ResourceViewHelper.java
+++ app/src/main/java/com/jaspersoft/android/jaspermobile/activities/repository/adapter/ResourceViewHelper.java
@@ -57,7 +57,7 @@ public class ResourceViewHelper {
     public static int getResourceIcon(ResourceLookup.ResourceType resourceType) {
         switch (resourceType) {
             case folder:
-                return R.drawable.sample_repo_violet;
+                return R.drawable.sample_repo_grey;
             case dashboard:
                 return R.drawable.sample_dashboard_blue;
             case reportUnit:

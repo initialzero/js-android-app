08cf70e08d0c1dbc1a84c6ccd147716c988f4e78
Tom Koptel <tom.koptel@gmail.com>
2014-12-30 10:19:10 +0200

    Embedding JsXmlSpiceService

---
+++ app/src/main/java/com/jaspersoft/android/jaspermobile/network/JsXmlSpiceService.java
@@ -0,0 +1,29 @@
+package com.jaspersoft.android.jaspermobile.network;
+
+import android.app.Application;
+
+import com.octo.android.robospice.SpiceService;
+import com.octo.android.robospice.persistence.CacheManager;
+import com.octo.android.robospice.persistence.exception.CacheCreationException;
+import com.octo.android.robospice.persistence.springandroid.xml.SimpleSerializerObjectPersisterFactory;
+import com.octo.android.robospice.persistence.string.InFileStringObjectPersister;
+
+/**
+ * This class offers a {@link SpiceService} dedicated to xml web services. Provides
+ * caching.
+ *
+ * @author Ivan Gadzhega
+ * @since 1.6
+ */
+public class JsXmlSpiceService extends SpiceService {
+
+    @Override
+    public CacheManager createCacheManager(Application application) throws CacheCreationException {
+        CacheManager cacheManager = new CacheManager();
+        // It is really important to keep proper persister order.
+        cacheManager.addPersister(new InFileStringObjectPersister(application));
+        cacheManager.addPersister(new SimpleSerializerObjectPersisterFactory(application));
+        return cacheManager;
+    }
+
+}

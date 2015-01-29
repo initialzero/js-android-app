7d8c1d9cbab3a10daee4a23d12d37ea89563553d
Tom Koptel <tom.koptel@gmail.com>
2015-01-29 14:48:55 +0200

    Updating ServerRelease version

--- sdk/src/main/java/com/jaspersoft/android/retrofit/sdk/server/ServerRelease.java
+++ sdk/src/main/java/com/jaspersoft/android/retrofit/sdk/server/ServerRelease.java
@@ -34,7 +34,9 @@ public enum ServerRelease {
     EMERALD_MR1(5.2d),
     EMERALD_MR2(5.5d),
     EMERALD_MR3(5.6d),
-    AMBER(6.0d);
+    AMBER(6.0d),
+    AMBER_MR1(6.01d),
+    JADE(6.1d);
 
     private final double mVersionCode;
 

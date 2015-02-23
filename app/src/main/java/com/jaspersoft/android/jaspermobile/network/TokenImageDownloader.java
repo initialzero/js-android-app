528076bdfafd195514e5d544c19f903fdc4f9f4c
Tom Koptel <tom.koptel@gmail.com>
2015-02-23 15:26:35 +0200

    Removing uil package

--- app/src/main/java/com/jaspersoft/android/jaspermobile/network/TokenImageDownloader.java
+++ app/src/main/java/com/jaspersoft/android/jaspermobile/network/TokenImageDownloader.java
@@ -22,7 +22,7 @@
  * <http://www.gnu.org/licenses/lgpl>.
  */
 
-package com.jaspersoft.android.jaspermobile.uil;
+package com.jaspersoft.android.jaspermobile.network;
 
 import android.content.Context;
 

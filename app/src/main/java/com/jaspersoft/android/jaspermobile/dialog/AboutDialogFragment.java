75ec176ca1a98a9d094e34695ae51a2c5302f9bf
Andrew Tivodar <andrewtivodar@yahoo.com>
2015-03-11 17:01:29 +0200

    Feedback confirmation dialog removed

--- app/src/main/java/com/jaspersoft/android/jaspermobile/dialog/AboutDialogFragment.java
+++ app/src/main/java/com/jaspersoft/android/jaspermobile/dialog/AboutDialogFragment.java
@@ -1,4 +1,4 @@
-package com.jaspersoft.android.jaspermobile.activities.navigation;
+package com.jaspersoft.android.jaspermobile.dialog;
 
 import android.app.AlertDialog;
 import android.app.Dialog;

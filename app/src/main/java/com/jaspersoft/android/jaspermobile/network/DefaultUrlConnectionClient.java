9f6f07aaf9e2af905bacbc944575a32cbbf27b51
Tom Koptel <tom.koptel@gmail.com>
2015-05-20 15:25:57 +0300

    Implement default url connection client for Retrofit

---
+++ app/src/main/java/com/jaspersoft/android/jaspermobile/network/DefaultUrlConnectionClient.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright © 2014 TIBCO Software, Inc. All rights reserved.
+ * http://community.jaspersoft.com/project/jaspermobile-android
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of Jaspersoft Mobile for Android.
+ *
+ * Jaspersoft Mobile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jaspersoft Mobile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Jaspersoft Mobile for Android. If not, see
+ * <http://www.gnu.org/licenses/lgpl>.
+ */
+
+package com.jaspersoft.android.jaspermobile.network;
+
+import android.content.Context;
+
+import com.jaspersoft.android.jaspermobile.util.DefaultPrefHelper;
+import com.jaspersoft.android.jaspermobile.util.DefaultPrefHelper_;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
+import retrofit.client.Request;
+import retrofit.client.UrlConnectionClient;
+import timber.log.Timber;
+
+/**
+ * @author Tom Koptel
+ * @since 2.0
+ */
+public class DefaultUrlConnectionClient extends UrlConnectionClient {
+    private final int mConnectionReadTimeOut;
+    private final int mConnectionTimeOut;
+
+    public DefaultUrlConnectionClient(Context context) {
+        DefaultPrefHelper defaultPrefHelper = DefaultPrefHelper_.getInstance_(context);
+        mConnectionReadTimeOut = defaultPrefHelper.getReadTimeoutValue();
+        mConnectionTimeOut = defaultPrefHelper.getConnectTimeoutValue();
+    }
+
+    @Override
+    protected HttpURLConnection openConnection(Request request) {
+        HttpURLConnection connection = null;
+        try {
+            connection = super.openConnection(request);
+            connection.setConnectTimeout(mConnectionTimeOut);
+            connection.setReadTimeout(mConnectionReadTimeOut);
+        } catch (IOException e) {
+            Timber.e(e, e.getMessage());
+        }
+        return connection;
+    }
+}

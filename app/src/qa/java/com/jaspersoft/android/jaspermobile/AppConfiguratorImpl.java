723b61fb5e5638d32155a94789ef9b15543414f2
Tom Koptel <tom.koptel@gmail.com>
2015-05-22 15:19:55 +0300

    Fixing production/qa flavor issues

--- app/src/qa/java/com/jaspersoft/android/jaspermobile/AppConfiguratorImpl.java
+++ app/src/qa/java/com/jaspersoft/android/jaspermobile/AppConfiguratorImpl.java
@@ -3,6 +3,7 @@ package com.jaspersoft.android.jaspermobile;
 import android.content.Context;
 
 import com.crashlytics.android.Crashlytics;
+import com.jaspersoft.android.jaspermobile.util.DefaultPrefHelper_;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -13,6 +14,7 @@ import io.fabric.sdk.android.Fabric;
 public class AppConfiguratorImpl implements AppConfigurator {
     @Override
     public void configCrashAnalytics(Context appContext) {
+        DefaultPrefHelper_ prefHelper = DefaultPrefHelper_.getInstance_(appContext);
         Fabric.with(appContext, new Crashlytics());
         if (prefHelper.sendCrashReports()) {
             Fabric.with(appContext, new Crashlytics());

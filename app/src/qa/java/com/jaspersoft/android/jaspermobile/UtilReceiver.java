20832a4708c622ad8e07178e08033de3726822fe
Tom Koptel <tom.koptel@gmail.com>
2015-05-25 15:29:06 +0300

    Add QA features for downgrading server

--- app/src/qa/java/com/jaspersoft/android/jaspermobile/UtilReceiver.java
+++ app/src/qa/java/com/jaspersoft/android/jaspermobile/UtilReceiver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2015 TIBCO Software, Inc. All rights reserved.
+ * Copyright (c) 2015 TIBCO Software, Inc. All rights reserved.
  * http://community.jaspersoft.com/project/jaspermobile-android
  *
  * Unless you have purchased a commercial license agreement from Jaspersoft,
@@ -29,8 +29,10 @@ import android.accounts.AccountManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.text.TextUtils;
 import android.widget.Toast;
 
+import com.jaspersoft.android.retrofit.sdk.account.AccountServerData;
 import com.jaspersoft.android.retrofit.sdk.account.JasperAccountManager;
 import com.jaspersoft.android.retrofit.sdk.util.JasperSettings;
 
@@ -42,6 +44,8 @@ public class UtilReceiver extends BroadcastReceiver {
     private static final String REMOVE_COOKIES = "jaspermobile.util.action.REMOVE_COOKIES";
     private static final String DEPRECATE_COOKIES = "jaspermobile.util.action.DEPRECATE_COOKIES";
     private static final String REMOVE_ALL_ACCOUNTS = "jaspermobile.util.action.REMOVE_ALL_ACCOUNTS";
+    private static final String DOWNGRADE_SERVER_VERSION = "jaspermobile.util.action.DOWNGRADE_SERVER_VERSION";
+    private static final String CHANGE_SERVER_EDITION = "jaspermobile.util.action.CHANGE_SERVER_EDITION";
 
     private static final String INVALID_COOKIE = "JSESSIONID=5513E1DE5437AE6B9F41CC5C8309B153; " +
             "Path=/jasperserver-pro/; HttpOnlyuserLocale=en_US;Expires=Sat, 23-May-2015 09:15:46 GMT;HttpOnly";
@@ -55,6 +59,12 @@ public class UtilReceiver extends BroadcastReceiver {
             overrideTokenWithOldOne(context);
         } else if (action.equals(REMOVE_ALL_ACCOUNTS)) {
             removeAccounts(context);
+        } else if (action.equals(DOWNGRADE_SERVER_VERSION)) {
+            downgradeServerVersion(context, intent);
+            overrideTokenWithOldOne(context);
+        } else if (action.equals(CHANGE_SERVER_EDITION)) {
+            changeServerVersion(context, intent);
+            overrideTokenWithOldOne(context);
         }
     }
 
@@ -81,5 +91,37 @@ public class UtilReceiver extends BroadcastReceiver {
             accountManager.removeAccount(account, null, null);
         }
         Toast.makeText(context, "Accounts removed", Toast.LENGTH_LONG).show();
+    }
+
+    private void downgradeServerVersion(Context context, Intent intent) {
+        Account account = JasperAccountManager.get(context).getActiveAccount();
+        if (account == null) {
+            Toast.makeText(context, "No active account. Nothing to downgrade.", Toast.LENGTH_LONG).show();
+        } else {
+            String versionName = intent.getStringExtra("target_version");
+            if (TextUtils.isEmpty(versionName)) {
+                Toast.makeText(context, "Target version missing. Can't downgrade.", Toast.LENGTH_LONG).show();
+            } else {
+                AccountManager accountManager = AccountManager.get(context);
+                accountManager.setUserData(account, AccountServerData.VERSION_NAME_KEY, versionName);
+                Toast.makeText(context, "Server was downgraded to version: " + versionName, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void changeServerVersion(Context context, Intent intent) {
+        Account account = JasperAccountManager.get(context).getActiveAccount();
+        if (account == null) {
+            Toast.makeText(context, "No active account. No way to change edition.", Toast.LENGTH_LONG).show();
+        } else {
+            String editionName = intent.getStringExtra("edition_version");
+            if (TextUtils.isEmpty(editionName)) {
+                Toast.makeText(context, "Target server edition not found. Can't update server.", Toast.LENGTH_LONG).show();
+            } else {
+                AccountManager accountManager = AccountManager.get(context);
+                accountManager.setUserData(account, AccountServerData.EDITION_KEY, editionName);
+                Toast.makeText(context, "Server edition was changed: " + editionName, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }

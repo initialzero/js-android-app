53dbef066211bffce9fac8207fe35865aa7e45db
Tom Koptel <tom.koptel@gmail.com>
2015-06-23 11:30:36 +0300

    Implementing FeedbackMessage

---
+++ app/src/main/java/com/jaspersoft/android/jaspermobile/util/feedback/FeedbackMessage.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright © 2015 TIBCO Software, Inc. All rights reserved.
+ * http://community.jaspersoft.com/project/jaspermobile-android
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of Jaspersoft Mobile for Android.
+ *
+ * Jaspersoft Mobile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jaspersoft Mobile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Jaspersoft Mobile for Android. If not, see
+ * <http://www.gnu.org/licenses/lgpl>.
+ */
+
+package com.jaspersoft.android.jaspermobile.util.feedback;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+
+import com.jaspersoft.android.jaspermobile.R;
+import com.jaspersoft.android.jaspermobile.util.server.ServerInfoProvider;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @author Tom Koptel
+ * @since 2.1
+ */
+final class FeedbackMessage {
+    private final ServerInfoProvider mInfoProvider;
+    private final Context mContext;
+    private final StringBuilder mTextMessage;
+    private final List<String> mParts;
+
+    FeedbackMessage(Context context, ServerInfoProvider infoProvider) {
+        mContext = context;
+        mInfoProvider = infoProvider;
+        mTextMessage = new StringBuilder();
+        mParts = new ArrayList<String>();
+    }
+
+    @NonNull
+    public String getMessage() {
+        return append(generateAppVersionInfo())
+                .append(generateServerVersion())
+                .append(generateServerEdition())
+                .createText()
+                .toString();
+    }
+
+    private StringBuilder createText() {
+        Iterator<String> iterator = mParts.iterator();
+        while (iterator.hasNext()) {
+            mTextMessage.append(iterator.next());
+            if (iterator.hasNext()) {
+                mTextMessage.append("\n");
+            }
+        }
+        return mTextMessage;
+    }
+
+    private FeedbackMessage append(String message) {
+        if (!TextUtils.isEmpty(message)) {
+            mParts.add(message);
+        }
+        return this;
+    }
+
+    private String generateServerVersion() {
+        String serverVersion = mInfoProvider.getServerVersion();
+        if (TextUtils.isEmpty(serverVersion)) {
+            return null;
+        } else {
+            return mContext.getString(R.string.jrs_version_data, serverVersion);
+        }
+    }
+
+    private String generateServerEdition() {
+        String serverEdition = mInfoProvider.getServerEdition();
+        if (TextUtils.isEmpty(serverEdition)) {
+            return null;
+        } else {
+            return mContext.getString(R.string.jrs_edition_data, serverEdition);
+        }
+    }
+
+    private String generateAppVersionInfo() {
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = mContext.getPackageManager().getPackageInfo(mContext.getPackageName(), 0);
+
+            String versionName = packageInfo.versionName;
+            String versionCode = String.valueOf(packageInfo.versionCode);
+
+            return  mContext.getString(R.string.jasper_app_data, versionName, versionCode);
+        } catch (PackageManager.NameNotFoundException e) {
+            return null;
+        }
+    }
+}
---
+++ app/src/test/java/com/jaspersoft/android/jaspermobile/util/feedback/FeedbackMessageTest.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright © 2015 TIBCO Software, Inc. All rights reserved.
+ * http://community.jaspersoft.com/project/jaspermobile-android
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of Jaspersoft Mobile for Android.
+ *
+ * Jaspersoft Mobile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jaspersoft Mobile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Jaspersoft Mobile for Android. If not, see
+ * <http://www.gnu.org/licenses/lgpl>.
+ */
+
+package com.jaspersoft.android.jaspermobile.util.feedback;
+
+import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+import com.jaspersoft.android.jaspermobile.BuildConfig;
+import com.jaspersoft.android.jaspermobile.util.server.ServerInfoProvider;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
+import org.robolectric.res.builder.RobolectricPackageManager;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.when;
+import static org.robolectric.Shadows.shadowOf;
+
+/**
+ * @author Tom Koptel
+ * @since 2.1
+ */
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(sdk = 21, constants = BuildConfig.class)
+public class FeedbackMessageTest {
+    @Mock
+    ServerInfoProvider serverInfoProvider;
+
+    FeedbackMessage feedbackMessage;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+
+        Application context = RuntimeEnvironment.application;
+        RobolectricPackageManager packageManager = (RobolectricPackageManager) shadowOf(context).getPackageManager();
+        try {
+            PackageInfo info = packageManager.getPackageInfo(context.getPackageName(), 0);
+            info.versionCode = 20100000;
+            info.versionName = "2.1";
+        } catch (PackageManager.NameNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        feedbackMessage = new FeedbackMessage(RuntimeEnvironment.application, serverInfoProvider);
+    }
+
+    @Test
+    public void shouldCreateValidFeedbackMessage() {
+        when(serverInfoProvider.getServerVersion()).thenReturn("6.1");
+        when(serverInfoProvider.getServerEdition()).thenReturn("CE");
+
+        String message = feedbackMessage.getMessage();
+
+        assertThat(message, containsString("Version name: 2.1"));
+        assertThat(message, containsString("Version code: 20100000"));
+        assertThat(message, containsString("JRS version: 6.1"));
+        assertThat(message, containsString("JRS edition: CE"));
+    }
+
+    @Test
+    public void shouldOmitServerDataIfMissing() {
+        when(serverInfoProvider.getServerVersion()).thenReturn(null);
+        when(serverInfoProvider.getServerEdition()).thenReturn(null);
+
+        String message = feedbackMessage.getMessage();
+
+        assertThat(message, containsString("Version name: 2.1"));
+        assertThat(message, containsString("Version code: 20100000"));
+    }
+}

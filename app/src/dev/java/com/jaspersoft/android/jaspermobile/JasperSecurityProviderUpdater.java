75d2d44d2d2410f35f6e1e434cc58c8ff0b549f8
Andrew Tivodar <andrewtivodar@yahoo.com>
2015-08-27 16:41:18 +0300

    Disable Security Provider updater for dev builds

---
+++ app/src/dev/java/com/jaspersoft/android/jaspermobile/JasperSecurityProviderUpdater.java
@@ -0,0 +1,8 @@
+package com.jaspersoft.android.jaspermobile;
+
+/**
+ * @author Andrew Tivodar
+ * @since 2.2
+ */
+public class JasperSecurityProviderUpdater {
+}
---
+++ app/src/main/java/com/jaspersoft/android/jaspermobile/activities/SecurityProviderUpdater.java
@@ -0,0 +1,9 @@
+package com.jaspersoft.android.jaspermobile.activities;
+
+/**
+ * @author Andrew Tivodar
+ * @since 2.2
+ */
+public interface SecurityProviderUpdater {
+    void update();
+}

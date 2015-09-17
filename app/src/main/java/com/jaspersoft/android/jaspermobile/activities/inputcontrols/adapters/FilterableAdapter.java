32be6b407fd9efcd731be8c487e26c9291af20de
Andrew Tivodar <andrewtivodar@yahoo.com>
2015-09-17 12:28:23 +0300

    Update search algorithm

--- app/src/main/java/com/jaspersoft/android/jaspermobile/activities/inputcontrols/adapters/FilterableAdapter.java
+++ app/src/main/java/com/jaspersoft/android/jaspermobile/activities/inputcontrols/adapters/FilterableAdapter.java
@@ -1,28 +1,35 @@
 package com.jaspersoft.android.jaspermobile.activities.inputcontrols.adapters;
 
-import android.support.v7.util.SortedList;
 import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * RecyclerView adapter that support item filtering.
+ *
  * @author Andrew Tivodar
  * @since 2.2
  */
 public abstract class FilterableAdapter<VH extends RecyclerView.ViewHolder, IT> extends RecyclerView.Adapter<VH> {
 
-    private SortedList<Integer> mAppropriateItemsPositionList;
+    private List<IT> mFilteredItemList;
     private List<IT> mItemsList;
     private String mFilterWord;
 
+    /**
+     * Create adapter using provided data set.
+     *
+     * @param valuesList item data set. Can not be null.
+     */
     public FilterableAdapter(List<IT> valuesList) {
         if (valuesList == null) {
             throw new IllegalArgumentException("Items list can not be null");
         }
         this.mFilterWord = "";
         this.mItemsList = valuesList;
-        this.mAppropriateItemsPositionList = new SortedList<>(Integer.class, new FilterableListSortCallback());
-        updateAppropriateItemsList();
+        this.mFilteredItemList = new ArrayList<>();
+        mFilteredItemList.addAll(mItemsList);
     }
 
     /**
@@ -32,38 +39,53 @@ public abstract class FilterableAdapter<VH extends RecyclerView.ViewHolder, IT> 
      */
     @Override
     public final int getItemCount() {
-        return mAppropriateItemsPositionList.size();
-    }
-
-    public final IT getItem(int position) {
-        int positionInList = mAppropriateItemsPositionList.get(position);
-        return mItemsList.get(positionInList);
-    }
-
-    public final int getItemPosition(int adapterPosition){
-        return mAppropriateItemsPositionList.get(adapterPosition);
+        return mFilteredItemList.size();
     }
 
     /**
-     * Items list will be filtered with filter word.
-     * For disabling filtering pass empty String as argument.
-     * @param filterWord Word to filter with.
+     * Returns item from the data set by index in filtered data set.
+     *
+     * @param position Adapter index - index in filtered data set, not in full one.
+     * @return item from data set
+     */
+    public final IT getItem(int position) {
+        return mFilteredItemList.get(position);
+    }
+
+    /**
+     * Convert filtered position to position in full data set.
+     *
+     * @param adapterPosition Position in filtered data set.
+     * @return Position in full data set.
+     */
+    public final int getItemPosition(int adapterPosition) {
+        return mItemsList.indexOf(mFilteredItemList.get(adapterPosition));
+    }
+
+    /**
+     * Update item state. Call this instead of {@link #notifyItemChanged(int) notifyItemChanged()}.
+     *
+     * @param position Item position in data set.
+     */
+    public void updateItem(int position) {
+        int filteredIndex = mFilteredItemList.indexOf(mItemsList.get(position));
+        if (filteredIndex != -1) {
+            notifyItemChanged(filteredIndex);
+        }
+    }
+
+    /**
+     * Filters data set with filter word.
+     * For disabling filtering pass empty {@link String String} as argument.
+     *
+     * @param filterWord Word to filter with. Can not be null.
      */
     public void filter(String filterWord) {
         if (filterWord == null) {
             throw new IllegalArgumentException("Filter word can not be null");
         }
+        filterList(filterWord.toLowerCase());
         mFilterWord = filterWord.toLowerCase();
-        updateAppropriateItemsList();
-    }
-
-    /**
-     * Update item state. Call this instead of {@link #notifyItemChanged(int) notifyItemChanged()}.
-     * @param position Item position in list.
-     */
-    public void updateItem(int position){
-        int adapterPosition = mAppropriateItemsPositionList.indexOf(position);
-        notifyItemChanged(adapterPosition);
     }
 
     /**
@@ -74,64 +96,31 @@ public abstract class FilterableAdapter<VH extends RecyclerView.ViewHolder, IT> 
      */
     protected abstract String getValueForFiltering(IT item);
 
-    private void updateAppropriateItemsList() {
-        if (mFilterWord.isEmpty()) {
-            initFullList();
-            return;
-        }
+    private void filterList(String newFilterWord){
+        List<IT> newFilteredList = new ArrayList<>();
+        int alreadyRemoved = 0;
 
-        mAppropriateItemsPositionList.beginBatchedUpdates();
-        for (int i = 0; i < mItemsList.size(); i++) {
-            String itemValue = getValueForFiltering(mItemsList.get(i)).toLowerCase();
-            if (itemValue.contains(mFilterWord)) {
-                mAppropriateItemsPositionList.add(i);
-            } else {
-                mAppropriateItemsPositionList.remove(i);
+        for (int i = 0; i < mFilteredItemList.size(); i++) {
+            IT item = mFilteredItemList.get(i);
+            String valueForFiltering = getValueForFiltering(item).toLowerCase();
+            boolean valueContainsNewFilterWord = valueForFiltering.contains(newFilterWord);
+            if (!valueContainsNewFilterWord) {
+                notifyItemRemoved(i - alreadyRemoved);
+                alreadyRemoved++;
             }
         }
-        mAppropriateItemsPositionList.endBatchedUpdates();
-    }
+        for (IT item : mItemsList) {
+            String valueForFiltering = getValueForFiltering(item).toLowerCase();
+            boolean valueContainsNewFilterWord = valueForFiltering.contains(newFilterWord);
+            boolean valueContainsOldFilterWord = valueForFiltering.contains(mFilterWord);
 
-    private void initFullList() {
-        for (int i = 0; i < mItemsList.size(); i++) {
-            mAppropriateItemsPositionList.add(i);
+            if (valueContainsNewFilterWord) {
+                newFilteredList.add(item);
+                if (!valueContainsOldFilterWord) {
+                    notifyItemInserted(newFilteredList.size() - 1);
+                }
+            }
         }
-    }
-
-    private class FilterableListSortCallback extends SortedList.Callback<Integer> {
-        @Override
-        public int compare(Integer o1, Integer o2) {
-            return o1.compareTo(o2);
-        }
-
-        @Override
-        public void onInserted(int position, int count) {
-            notifyItemRangeInserted(position, count);
-        }
-
-        @Override
-        public void onRemoved(int position, int count) {
-            notifyItemRangeRemoved(position, count);
-        }
-
-        @Override
-        public void onMoved(int fromPosition, int toPosition) {
-            notifyItemMoved(fromPosition, toPosition);
-        }
-
-        @Override
-        public void onChanged(int position, int count) {
-            notifyItemRangeChanged(position, count);
-        }
-
-        @Override
-        public boolean areContentsTheSame(Integer oldItem, Integer newItem) {
-            return oldItem.equals(newItem);
-        }
-
-        @Override
-        public boolean areItemsTheSame(Integer item1, Integer item2) {
-            return item1.equals(item2);
-        }
+        mFilteredItemList = newFilteredList;
     }
 }

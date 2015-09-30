58f22e992b3dad3cb4ee8eaeeaa11705a0802cb6
Tom Koptel <tom.koptel@gmail.com>
2015-09-30 13:46:01 +0300

    Firing refresh on dashboard component specifically

--- app/src/main/assets/dashboard-amber2-android-mobilejs-sdk.js
+++ app/src/main/assets/dashboard-amber2-android-mobilejs-sdk.js
@@ -128,11 +128,7 @@ void 0===c?d&&"get"in d&&null!==(e=d.get(a,b))?e:(e=n.find.attr(a,b),null==e?voi
       };
 
       DashboardController.prototype.refresh = function() {
-        return this.components.forEach((function(_this) {
-          return function(component) {
-            return _this.dashboard.refresh(component.id).fail(_this._processErrors);
-          };
-        })(this));
+        return this.dashboard.refresh().fail(this._processErrors);
       };
 
       DashboardController.prototype.minimizeDashlet = function() {

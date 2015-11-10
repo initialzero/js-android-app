ccc2f2797281213c003ec18e9f22a2ecb07bc6ca
Tom Koptel <tom.koptel@gmail.com>
2015-11-10 19:34:30 +0200

    Implmenet JasperServerRepository saveCredentials case

---
+++ app/src/main/java/com/jaspersoft/android/jaspermobile/data/repository/JasperServerDataRepository.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright © 2015 TIBCO Software, Inc. All rights reserved.
+ * http://community.jaspersoft.com/project/jaspermobile-android
+ *
+ * Unless you have purchased a commercial license agreement from TIBCO Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of TIBCO Jaspersoft Mobile for Android.
+ *
+ * TIBCO Jaspersoft Mobile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TIBCO Jaspersoft Mobile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with TIBCO Jaspersoft Mobile for Android. If not, see
+ * <http://www.gnu.org/licenses/lgpl>.
+ */
+
+package com.jaspersoft.android.jaspermobile.data.repository;
+
+import com.jaspersoft.android.jaspermobile.data.cache.JasperServerCache;
+import com.jaspersoft.android.jaspermobile.domain.Profile;
+import com.jaspersoft.android.jaspermobile.domain.repository.JasperServerRepository;
+import com.jaspersoft.android.jaspermobile.domain.server.JasperServer;
+import com.jaspersoft.android.jaspermobile.internal.di.PerActivity;
+
+import javax.inject.Inject;
+
+/**
+ * @author Tom Koptel
+ * @since 2.3
+ */
+@PerActivity
+public final class JasperServerDataRepository implements JasperServerRepository {
+    private final JasperServerCache mJasperServerCache;
+
+    @Inject
+    public JasperServerDataRepository(JasperServerCache jasperServerCache) {
+        mJasperServerCache = jasperServerCache;
+    }
+
+    @Override
+    public void saveServer(Profile profile, JasperServer jasperServer) {
+        mJasperServerCache.put(profile, jasperServer);
+    }
+}

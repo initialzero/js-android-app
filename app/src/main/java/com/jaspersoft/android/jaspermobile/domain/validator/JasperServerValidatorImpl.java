a39216e389a9c156127cc57774c4dbe56bbb1cbc
Tom Koptel <tom.koptel@gmail.com>
2015-11-10 11:27:19 +0200

    Implementing Server validator

---
+++ app/src/main/java/com/jaspersoft/android/jaspermobile/domain/validator/JasperServerValidatorImpl.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright © 2015 TIBCO Software, Inc. All rights reserved.
+ * http://community.jaspersoft.com/project/jaspermobile-android
+ *
+ * Unless you have purchased a commercial license agreement from TIBCO Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of TIBCO Jaspersoft Mobile for Android.
+ *
+ * TIBCO Jaspersoft Mobile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TIBCO Jaspersoft Mobile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with TIBCO Jaspersoft Mobile for Android. If not, see
+ * <http://www.gnu.org/licenses/lgpl>.
+ */
+
+package com.jaspersoft.android.jaspermobile.domain.validator;
+
+import com.jaspersoft.android.jaspermobile.data.server.JasperServer;
+import com.jaspersoft.android.jaspermobile.data.validator.JasperServerValidator;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ * @author Tom Koptel
+ * @since 2.3
+ */
+@Singleton
+public final class JasperServerValidatorImpl implements JasperServerValidator {
+    @Inject
+    public JasperServerValidatorImpl() {}
+
+    @Override
+    public boolean validate(JasperServer instance) {
+        double version = instance.getVersion();
+        return version >= 5.5d;
+    }
+}

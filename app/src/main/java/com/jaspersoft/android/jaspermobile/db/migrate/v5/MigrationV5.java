744336f79765b2f111e11e16949273c5ad617b47
andrewtivodar <andrewtivodar@yahoo.com>
2015-12-11 17:02:25 +0200

    Added migration for saved items to v6

--- app/src/main/java/com/jaspersoft/android/jaspermobile/db/migrate/v5/MigrationV5.java
+++ app/src/main/java/com/jaspersoft/android/jaspermobile/db/migrate/v5/MigrationV5.java
@@ -22,43 +22,37 @@
  * <http://www.gnu.org/licenses/lgpl>.
  */
 
-package com.jaspersoft.android.jaspermobile.db;
+package com.jaspersoft.android.jaspermobile.db.migrate.v5;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.provider.Settings;
+import android.util.Base64;
 
-import com.jaspersoft.android.jaspermobile.db.database.JasperMobileDbDatabase;
-import com.jaspersoft.android.jaspermobile.db.migrate.v2.MigrationV2;
-import com.jaspersoft.android.jaspermobile.db.migrate.v3.MigrationV3;
-import com.jaspersoft.android.jaspermobile.db.migrate.v4.MigrationV4;
-import com.jaspersoft.android.jaspermobile.db.migrate.v5.MigrationV5;
+import com.jaspersoft.android.jaspermobile.R;
+import com.jaspersoft.android.jaspermobile.db.migrate.Migration;
+
+import java.io.UnsupportedEncodingException;
+import java.security.spec.AlgorithmParameterSpec;
+import java.security.spec.KeySpec;
+import java.util.Arrays;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.PBEParameterSpec;
 
 /**
- * @author Tom Koptel
- * @since 1.9
+ * @author Andrew Tivodar
+ * @since 2.3
  */
-public class JSDatabaseHelper extends JasperMobileDbDatabase {
-    private final Context mContext;
-
-    public JSDatabaseHelper(Context context) {
-        super(context);
-        mContext = context;
-    }
+public final class MigrationV5 implements Migration {
 
     @Override
-    public void onUpgrade(final SQLiteDatabase db, final int oldVersion, final int newVersion) {
-        executePragmas(db);
-        switch (oldVersion) {
-            case 1:
-            case 2:
-                new MigrationV2().migrate(db);
-            case 3:
-                new MigrationV3(mContext).migrate(db);
-            case 4:
-                new MigrationV4(mContext).migrate(db);
-            case 5:
-                new MigrationV5().migrate(db);
-                break;
-        }
+    public void migrate(SQLiteDatabase db) {
+        db.execSQL("ALTER TABLE saved_items ADD COLUMN downloaded NUMERIC DEFAULT 1");
     }
 }

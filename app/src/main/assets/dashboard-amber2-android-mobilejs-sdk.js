0fd9025b773edf2516dee2e83572ceeaafd923e3
Andrew Tivodar <andrewtivodar@yahoo.com>
2016-08-08 16:58:02 +0300

    Fixed problem with cutted chart type select screen (#503)

--- app/src/main/assets/dashboard-amber2-android-mobilejs-sdk.js
+++ app/src/main/assets/dashboard-amber2-android-mobilejs-sdk.js
@@ -793,9 +793,7 @@ void 0===c?d&&"get"in d&&null!==(e=d.get(a,b))?e:(e=n.find.attr(a,b),null==e?voi
       ScaleStyleDashboard.prototype.applyFor = function(factor) {
         var originalDashletInScaledCanvasCss, scaledCanvasCss;
         jQuery("#scale_style").remove();
-        scaledCanvasCss = ".scaledCanvas { transform-origin: 0 0 0; -ms-transform-origin: 0 0 0; -webkit-transform-origin: 0 0 0; transform: scale( " + factor + " ); -ms-transform: scale( " + factor + " ); -webkit-transform: scale( " + factor + " ); width: " + (100 / factor) + "% !important; height: " + (100 / factor) + "% !important; }";
-        originalDashletInScaledCanvasCss = ".dashboardCanvas > .content > .body div.canvasOverlay.originalDashletInScaledCanvas { transform-origin: 0 0 0; -ms-transform-origin: 0 0 0; -webkit-transform-origin: 0 0 0; transform: scale( " + (1 / factor) + " ); -ms-transform: scale( " + (1 / factor) + " ); -webkit-transform: scale( " + (1 / factor) + " ); width: " + (100 * factor) + "% !important; height: " + (100 * factor) + "% !important; }";
-        jQuery('<style id="scale_style"></style>').text(scaledCanvasCss + originalDashletInScaledCanvasCss).appendTo('head');
+        $('meta[name=viewport]').attr("content", "initial-scale=" + factor + ", width=device-width, height=device-height, user-scalable=yes, minimum-scale=0.1, maximum-scale=3");
       };
 
       return ScaleStyleDashboard;
